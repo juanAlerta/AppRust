7d3d8c453256a58f002b1df6195f97be15096920
NagashL0l <juan.blanco.martin@alumnos.uva.es>
2022-02-16 17:58:57 +0100

    first commit

--- target/debug/deps/app-9ca83ffd4fffd6fc.d
+++ target/debug/deps/app-9ca83ffd4fffd6fc.d
@@ -1,5 +1,5 @@
-c:\Users\metri\Projects\Tooth\app\target\debug\deps\app-9ca83ffd4fffd6fc.rmeta: src\main.rs
+c:\Users\metri\Projects\Tooth\AppRust\target\debug\deps\app-9ca83ffd4fffd6fc.rmeta: src\main.rs
 
-c:\Users\metri\Projects\Tooth\app\target\debug\deps\app-9ca83ffd4fffd6fc.d: src\main.rs
+c:\Users\metri\Projects\Tooth\AppRust\target\debug\deps\app-9ca83ffd4fffd6fc.d: src\main.rs
 
 src\main.rs:
