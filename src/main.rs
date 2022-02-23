561b501c235cd7f91f10751a8124e4330be75acd
Juan Blanco Martín <metridxblack@gmail.com>
2022-02-23 20:12:01 +0100

    parse config 1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use std::env;
+use std::{env, num::ParseIntError};
 //use std::fs;
-
 enum Argument_primary_type {
     Time,
     Notime,
@@ -10,9 +9,10 @@ enum Argument_primary_type {
 enum Argument_secundary_type {
     Dir,
     Show,
+    Zero
 }
 
-struct Arguments {
+struct Argument {
     primary_type: Argument_primary_type,
     secundary_type: Argument_secundary_type,
     extra_type: Argument_secundary_type,
@@ -21,36 +21,49 @@ struct Arguments {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    args.remove(0); 
-    parse_config(&args); 
 
-    /* 
-    let contents = fs::read_to_string(filename)
-        .expect("Algo ha salido mal leyendo el fichero");
-    */
+    let mut args: Vec<String> = env::args().collect();
+    args.remove(0);
+   
+    parse_config(&args); 
 }
 
-// parse_config recoge lo argumentos introducidos
-fn parse_config(args: &[String]) {
+fn parse_config(args: &[String]) -> Argument {
 
-    //parametros
-    let arg_time = String::from("time");
-    let arg_notime = String::from("notime");
+    let ruta = String::from("C:/Users/Juan/Projects/Tooth/AppRust");
 
+    let primary_type = Argument_primary_type::Time;
+    let secundary_type = Argument_secundary_type::Zero;
+    let extra_type = Argument_secundary_type::Zero;
+    let time_atr = 0;
+    let dir_atr = ruta;
+
+
+    println!("VECTOR: {:?} ",args);
 
     for i in args {
-        println!("{}",i);
-        println!("{}",i.eq(&arg_time));
+        if i.as_str() == "time" {
+            println!("argumento time 🤠");
+        }
+        if i.as_str() == "notime" {
+            println!("argumento notime 🤑");
+        }
+        if i.as_str() == "dir" {
+            println!("argumento time 📝");
+        }
+   
+        
     }
+     
+    let arguments = Argument{
+        primary_type: primary_type,
+        secundary_type: secundary_type,
+        extra_type: extra_type,
+        time_atr: time_atr,
+        dir_atr: dir_atr,
+    };
 
-    /* 
-    let arg1 = &args[0];
-    let arg2 = &args[1];
-    let arg3 = &args[2];    
-    */
-
-    
+    arguments
     
 }
 
---
+++ target/debug/deps/app-52b50a57a5599cc6.d
@@ -0,0 +1,5 @@
+c:\Users\Juan\Projects\Tooth\AppRust\target\debug\deps\app-52b50a57a5599cc6.rmeta: src\main.rs
+
+c:\Users\Juan\Projects\Tooth\AppRust\target\debug\deps\app-52b50a57a5599cc6.d: src\main.rs
+
+src\main.rs:
