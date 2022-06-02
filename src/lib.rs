a7e0e1b91f84e5ef81b61c086860e7f062785743
Juan <juan@anysystems.es>
2022-06-02 18:17:00 +0200

    vale

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+use std::ffi::CString;
+
+pub trait evec_wrapper {
+    fn execv_wrapper(path: &CString, argv: &[CString]);
+    fn execve_wrapper(path: &CString, argv: &[CString], envp: &[CString]);
+
+}
---
+++ src/log.rs
@@ -0,0 +1,21 @@
+
+
+fn log_syscall_execv(filename: &CString, argv: [&CString]) {
+
+    let envp: [&CString] = {Null};
+    log_syscall_exec(filename, argv, envp);
+}
+
+fn log_syscall_execve(filename: &CString, argv: [&CString], envp: [&CString]) {
+
+    log_syscall_exec(filename, argv, envp);
+}
+
+fn log_syscall_exec(filename: &CString, argv: [&CString], envp: [&CString]) {
+
+    let logMessage: &CString;
+    // let configuracion: Objeto Configuracion
+    init();
+    
+    
+}
