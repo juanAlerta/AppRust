ca051b4ad7fb4ea08cfabb9007a59d985838b565
Juan Blanco Martín <metridxblack@gmail.com>
2023-03-27 23:41:43 +0200

    Recorrer /proc

--- src/info_process_proc.rs
+++ src/info_process_proc.rs
@@ -1,10 +1,12 @@
-use std::fs;
-use std::path::Path;
+use std::fs::DirEntry;
+use std::{fs, path};
+use std::path::{Path, PathBuf};
+use clap::Error;
 use procfs::process::Process;
 
 pub fn proc_data() {
     let proc_path = Path::new("/proc");
-    for entry in fs::read_dir(proc_path).expect("Fallo al leer el directorio /proc") {
+    for entry in fs::read_dir(proc_path).expect("Can't access to /proc.") {
         if let Ok(entry) = entry {
             let path = entry.path();
             if path.is_dir() {
@@ -19,3 +21,18 @@ pub fn proc_data() {
 }
 
 
+pub fn leer_proc_data(){
+    //Recorre la carpeta
+    let path_folder: &path = Path::new("/proc");
+    
+    for entry in fs::read_dir(path).expect("Can't access to /proc.") {
+        let Ok(entry) = entry{
+            let path_file = entry.path(),
+
+
+
+        }
+    }
+
+
+}
