b8230cf64f9f07e700cd02a6ae086b0481279bb3
juanAlerta <metridxblack@gmail.com>
2023-05-07 14:12:32 +0200

    funcionando proc_data

--- src/info_process_proc.rs
+++ src/info_process_proc.rs
@@ -12,7 +12,7 @@ pub fn proc_data() {
             if path.is_dir() {
                 if let Ok(pid) = path.file_name().unwrap().to_str().unwrap().parse::<i32>() {
                     if let Ok(process) = Process::new(pid) {
-                        println!("PID: {}, Nombre: {}, Tamaño: {}", process.stat.pid, process.stat.comm, process.stat.size);
+                        println!("pid: {:?}, user: {:?}, timestamp: {:?}", process.pid, process., process.pid);
                     }
                 }
             }
@@ -20,19 +20,19 @@ pub fn proc_data() {
     }
 }
 
-
-pub fn leer_proc_data(){
-    //Recorre la carpeta
-    let path_folder: &path = Path::new("/proc");
-    
-    for entry in fs::read_dir(path).expect("Can't access to /proc.") {
-        let Ok(entry) = entry{
-            let path_file = entry.path(),
-
-
-
+pub fn proc_data2() {
+    let proc_path = Path::new("/proc");
+    for entry in fs::read_dir(proc_path).expect("Can't access to /proc.") {
+        if let Ok(entry) = entry {
+            let path = entry.path();
+            if path.is_dir() {
+                if let Ok(pid) = path.file_name().unwrap().to_str().unwrap().parse::<i32>() {
+                    if let Ok(process) = Process::new(pid) {
+                        println!("pid: {:?}", process.pid);
+                    }
+                }
+            }
         }
     }
-
-
 }
+
